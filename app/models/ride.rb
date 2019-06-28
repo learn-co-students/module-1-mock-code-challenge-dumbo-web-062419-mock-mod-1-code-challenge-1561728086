224ef1e4cdec68e9050f001356c665705a04d87c
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-06-28 11:10:59 -0400

    mock:(

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,35 @@
+# ride belongs to a passenger and a driver
+class Ride
+
+    attr_accessor :drive, :passenger, :distance
+
+    def initialize 
+    @driver = driver
+    @passenger = passenger
+    @distance = 
+    @@all 
+    end
+
+    def Ride.average_distance
+    #Returns the average distance across
+    #ALL rides
+    end 
+
+#Ride#passenger`
+    #Returns the Passenger object for that ride
+#Ride#driver`
+  #Returns the Driver object for that ride
+#Ride#distance`
+  #Returns the distance of the ride
+
+
+
+
+
+end
+# passenger1 = ride.new
+# driver1 = ride.new 
+
+
+
 
