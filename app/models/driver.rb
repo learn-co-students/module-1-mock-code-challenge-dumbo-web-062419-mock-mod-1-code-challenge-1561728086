5548d17819df52ac708e861191fa236cee15de3b
John Fajardo <fajardocj@gmail.com>
2019-06-28 11:07:09 -0400

    Didn't get to finish

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,40 @@
+class Driver
 
+# Driver#name
+# Returns the driver's name
+# Driver#passenger_names
+# Returns an array of all Passengers' names a driver has driven. The names should be unique (no repeats).
+# Driver#rides
+# Returns an array of all Rides a driver has made
+# Driver.all
+# Returns an array of all Drivers
+# Driver.mileage_cap(distance)
+# Takes an argument of a distance (float) and returns an array of all Drivers who have driven over the mileage
+
+    @@all = []
+    @rides = []
+
+    def initialize(name)
+        @name = name
+        @@all << name
+    end
+
+    def passenger_names
+    end
+
+    def add_ride(ride)
+        @rides << ride
+    end
+
+    def rides
+        @rides
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.mileage_cap(distance)
+    end
+
+end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,59 @@
+class Passenger
 
+# Passenger#name
+# Returns the name of the passenger
+# Passenger#rides
+# Returns an array of Ride instances that this person has been on
+# Passenger#drivers
+# Returns an array of Driver instances that this person has rode with
+# Passenger#total_distance
+# Returns the floating number that represents the total distance the passenger has travelled using the service
+# Passenger.all
+# Returns an array of all Passengers
+# Passenger.premium_members
+# Returns an array of all Passengers who have travelled over 100 miles in total with the service
+
+
+    attr_reader :name
+
+    @@all = []
+    @@premium_members = []
+
+    @rides = []
+    @drivers = []
+    @total_distance = 0
+
+
+    def initialize(name)
+        @name = name
+        @@all << name
+    end
+
+    def name
+        @name
+    end
+
+    def self.premium_members
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        @rides
+    end
+
+    def drivers
+        @drivers
+    end
+
+    def total_distance
+        @total_distance
+    end
+
+    def self.premium_members
+    end
+
+
+end
