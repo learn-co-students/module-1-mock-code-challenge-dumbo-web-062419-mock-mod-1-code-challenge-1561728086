1a4bbc9a231866bad5e7a94e65709ed0dabfc4db
github cjl248 <github cjl248@cornell.edu>
2019-06-28 11:07:51 -0400

    love some feedback pleasegit add .

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,26 @@
+class Driver
 
+    attr_reader :name, :passenger_names, :rides
+
+    @@all =[]
+    @@mileage_cap = []
+
+    def initialize(name)
+        @name = name
+        @passenger_names = []
+        @rides = []
+        #add to ride array of drivers when initialized
+        Ride.add_passenger(self)
+
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.mileage_cap(distance)
+        #need ride class first
+    end
+
+end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,30 @@
+class Passenger
+
+    attr_reader :name, :rides, :drivers, :total_distance
+
+    @@all = []
+    @@premium_members =[]
+
+    def initialize(name)
+        @name = name
+        @rides = []
+        @drivers = []
+        @total_distance = 0.0
+        #add to ride array of passengers when initialized
+        Ride.add_passenger(self)
+    end
+       
+    def self.all
+        @@all
+    end
+
+    def self.premium_members
+        @@all.each do |p|
+            if p.total_distance > 100
+                @@premium_members << premium_members
+            end
+        end
+    end
+
+end
 
