e3f4b7b5e7ebd47a53f9b49d92ff4b3fcb38182b
Rashad <rashad.walcott15@gmail.com>
2019-06-28 11:07:20 -0400

    Still working on this but understood more than I thought

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,27 @@
+class Passenger
+    attr_accessor :name , :driver, :rides, :total_distance
+    @@all = []
+    def initialize(name,rides,driver)
+        @name = name
+        @rides = rides
+        @@all << self
+    end
+    def self.all
+        @all
+    end
+def self.rides
+    rides = []
+    Ride.all.each do |ride|
+        rides << self
+    end
+    rides
+end
 
+
+
+
+
+
+
+
+end
