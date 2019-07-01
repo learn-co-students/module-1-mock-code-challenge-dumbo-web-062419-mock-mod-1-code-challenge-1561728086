61c0f347011ebb001d6f1d82752d402d007a693d
= <=>
2019-07-01 11:11:00 -0400

    review wth joe

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -10,18 +10,29 @@ class Passenger
     end
 
     def rides
-        #array of ride instances the person has been on
-        if Ride.passenger == self #problems here
-            Ride.all.select{|ride| ride}
-        end
+				#array of ride instances the person has been on
+        # if Ride.passenger == self #problems here
+        #     Ride.all.select{|ride| ride}
+				# end
+				
+				# review session solution
+				Ride.all.select { |ride| ride.passenger == self }
     end
 
     def drivers
-        #array of drivers they have had
+				# my_rides = Ride.all.select { |ride| ride.passenger == self }
+				# my_rides.map { |ride| ride.driver }
+
+				# this is the same thing as above
+				rides.map { |ride| ride.driver }
     end
 
-    def total_distance
-        #returns float that displays total distace they have traveled
+		def total_distance
+			# set a counter
+			# iterate through the rides, find the distance for that ride, add that distance to the counter
+			total_distance = 0
+			rides.each { |ride| total_distance += ride.distance }
+			total_distance
     end
 
     def self.all
@@ -29,7 +40,10 @@ class Passenger
     end
 
     def self.premium_members
-        #array of passengers how have traveled over 100 miles
+				#array of passengers how have traveled over 100 miles
+				@@all.select do |passenger|
+					passenger.total_distance > 100
+				end
     end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,15 @@ def reload
   load 'config/environment.rb'
 end
 
+passenger1 = Passenger.new("Joe")
+driver1 = Driver.new("Tony")
+driver2 = Driver.new("Eric")
+driver3 = Driver.new("Graham")
+driver4 = Driver.new("Kevin")
+
+ride1 = Ride.new(driver1, passenger1, 7.7)
+ride2 = Ride.new(driver2, passenger1, 1.7)
+ride3 = Ride.new(driver3, passenger1, 5.2)
 
 # Put your variables here~!
 
